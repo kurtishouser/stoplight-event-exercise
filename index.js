bcf5a57c6e9d1074d77692cee89c9c4f7764ddb6
Kurtis Houser <houserkurtis@gmail.com>
2017-04-29 15:27:18 -0700

    Complete exercise

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  let redLight = document.querySelector('#stopButton');
+  let yellowLight = document.querySelector('#slowButton');
+  let greenLight = document.querySelector('#goButton');
+  let buttons = document.querySelectorAll('.button');
+
+  redLight.addEventListener('click', function() {
+    stopLight.classList.toggle('stop');
+  });
+
+  yellowLight.addEventListener('click', function() {
+    slowLight.classList.toggle('slow');
+  });
+
+  greenLight.addEventListener('click', function() {
+    goLight.classList.toggle('go');
+  });
+
+  buttons.forEach((button) => button.addEventListener('mouseenter', function() {
+    console.log("One mouse enters the " + event.target.textContent + " button");
+  }));
+
+  buttons.forEach((button) => button.addEventListener('mouseleave', function() {
+    console.log("One mouse leaves the " + event.target.textContent + " button");
+  }));
+
 })();
